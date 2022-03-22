724adfa61e1d61578f6cfadce20186a7b114054b
mimansha-swarup <mimanshaswarup@gmail.com>
2022-03-22 17:57:38 +0530

    feat: product db

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,467 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    company: "Moda Rapido",
+    productName: "Men Maroon Printed Cotton Pure Cotton T-shirt",
+    price: 314,
+    discount: 65,
+    rating:3.7,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2378362/2018/6/9/270e0a7e-365b-4640-9433-b269c60bf3061528527188563-Moda-Rapido-Men-Maroon-Printed-Round-Neck-T-shirt-3811528527-1.jpg"
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    company: "Roadster",
+    productName: "Men White Brush Printed T-shirt",
+    price: 520,
+    discount: 60,
+    rating:2.2,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1824369/2017/5/5/11493980166188-Roadster-Men-White-Printed-Round-Neck-T-shirt-4411493980165893-1.jpg"
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    company: "HRX by Hrithik Roshan",
+    productName: "Men Black Slim Advanced Rapid Dry Raglan T-shirt",
+    price: 449,
+    discount: 45,
+    rating:1.3,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2314424/2018/5/24/16fad75c-35ed-4780-8625-193cc8dae4fc1527173672482-HRX-by-Hrithik-Roshan-Men-Black-Slim-Advanced-Rapid-Dry-Raglan-T-shirt-3541527173669891-1.jpg"
   },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "The Lifestyle Co Men Blue & White Striped Round Neck T-shirt",
+    price: 199,
+    discount: 60,
+    rating:4.2,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/9586559/2019/6/7/1d8f64f5-f8e3-43cc-a829-519066bc09221559911511214-Roadster-Men-Tshirts-9211559911509626-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Men White Printed Round Neck Pure Cotton T-shirt",
+    price: 699,
+    discount: 50,
+    rating:4.2,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12504232/2021/1/7/1148b24e-b1d9-4c38-9bb8-414607668a0b1610014269951-Roadster-Men-Tshirts-6171610014268376-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "WROGN",
+    productName: "Men Grey Melange Self Design Welt Pocket T-shirt",
+    price: 799,
+    discount: 40,
+    rating:4.1,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1894413/2017/6/20/11497943671766-WROGN-Men-Grey-Melange-Self-Design-Round-Neck-T-shirt-881497943671356-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "WROGN",
+    productName: "Men Black Printed Polo Collar T-shirt",
+    price: 824,
+    discount: 45,
+    rating:4.4,
+    categoryName: "T-Shirts",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2478324/2018/9/26/3bb86908-c651-4685-97b8-e3f49f79f5091537945068438-WROGN-Men-Black-Printed-Polo-Collar-T-shirt-2411537945068393-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Calvin Klein Jeans",
+    productName: "Men Blue Tapered Fit Light Fade Stretchable Jeans",
+    price: 599,
+    discount: 45,
+    rating:4.2,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16409240/2022/1/31/5cb77906-7c39-40e8-b5d8-c8e53e5dea3b1643630529078-Calvin-Klein-Jeans-Men-Jeans-5561643630528192-1.jpg"
+  },
+ 
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Men Black Slim Tapered Fit Mid-Rise Clean Look Stretchable Jeans",
+    price: 799,
+    discount: 60,
+    rating:3.8,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8962431/2019/12/23/1433f70a-8e66-4e61-94bb-7aa64e525ccd1577094332071-Roadster-Men-Black-Slim-Tapered-Fit-Mid-Rise-Clean-Look-Stre-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Men Black Slim Tapered Fit Mid-Rise Clean Look Stretchable Jeans",
+    price: 799,
+    discount: 60,
+    rating:3.8,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/8962431/2019/12/23/1433f70a-8e66-4e61-94bb-7aa64e525ccd1577094332071-Roadster-Men-Black-Slim-Tapered-Fit-Mid-Rise-Clean-Look-Stre-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "WROGN",
+    productName: "Men Blue Slim Fit Mid-Rise Clean Look Stretchable Jeans",
+    price: 599,
+    discount: 40,
+    rating:2.8,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/7229767/2019/1/31/97d63969-943d-46d3-b9e0-6bf57b6428821548938509728-WROGN-Men-Blue-Slim-Fit-Mid-Rise-Clean-Look-Stretchable-Jean-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Men Blue Skinny Fit Mid-Rise Clean Look Stretchable Jeans",
+    price: 849,
+    discount: 50,
+    rating:1.8,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10092097/2019/10/15/adebe2b5-fa5a-4f24-aa6b-318a220756c21571136170029-Roadster-Men-Jeans-2931571136167945-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "WROGN",
+    productName: "Men Blue Slim Fit Mid-Rise Jeans",
+    price: 299,
+    discount: 40,
+    rating:3.8,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1758520/2017/3/9/11489048805059-WROGN-Men-Blue-Slim-Fit-Mid-Rise-Clean-Look-Jeans-1581489048804772-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Men Blue Slim Fit Mid-Rise Jeans",
+    price: 934,
+    discount: 45,
+    rating:4.1,
+    categoryName: "Jeans",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10064547/2019/10/4/8ad05bbb-6936-4b70-9257-b11988a545841570182863686-Roadster-Men-Blue-Super-Skinny-Fit-Mid-Rise-Clean-Look-Stret-1.jpg"
+  },
+  
+  {
+    _id: uuid(),
+    company: "HRX by Hrithik Roshan",
+    productName: "Men Active Grey Core 1.0 Running Shoes",
+    price: 249,
+    discount: 70,
+    rating:4.1,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2154051/2018/5/21/afc1319b-e500-4104-81c1-69dd9f5b5a221526884266133-HRX-by-Hrithik-Roshan-Men-Grey-Running-Shoes-1281526884264427-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "ASIAN",
+    productName: "Men Turquoise Blue Mesh Non-Marking Running Shoes",
+    price: 849,
+    discount: 40,
+    rating:3.1,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16959618/2022/1/28/ea154935-5f27-46fd-9561-fac4a94890961643359928542ASIANMenTurquoiseBlue1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "ADIDAS",
+    productName: "Women White Self Design Ultraboost SRDY Running Shoes",
+    price: 849,
+    discount: 40,
+    rating:2.1,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/12031640/2021/12/3/06e012fc-f77a-4807-85c0-c2de1c5dd5471638521388854-ADIDAS-Women-White-Self-Design-Ultraboost-SRDY-Running-Shoes-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Quechua By Decathlon",
+    productName: "Women Brown High-Top Trekking Non-Marking Shoes",
+    price: 999,
+    discount: 40,
+    rating:.7,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14933858/2021/7/26/b5adbfcd-e29b-4fd1-bfee-56e62a1ff23e1627298223646WomenBrownWaterproofLeatherTrekkingBoots1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "HRX by Hrithik Roshan",
+    productName: "Men Blue Street Run 2.0 Running Shoe",
+    price: 99,
+    discount: 50,
+    rating:4.2,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11954114/2021/6/16/24a987b5-28fb-4359-9c53-7a7f229781bf1623846610205-HRX-by-Hrithik-Roshan-Men-Blue-Street-Run-20-Running-Shoe-58-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "HRX by Hrithik Roshan",
+    productName: "Men White And Fluorescent Green N-FLUX Running Shoe",
+    price: 399,
+    discount: 80,
+    rating:4.7,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11999704/2020/11/3/bae44908-30ed-4237-9fe8-dae29f8cabbb1604401775122-HRX-by-Hrithik-Roshan-Men-White-And-Fluorescent-Green-N-FLUX-1.jpg"
+  },
+  
+  {
+    _id: uuid(),
+    company: "HRX by Hrithik Roshan",
+    productName: "Men Grey Core Sports Shoe",
+    price: 499,
+    discount: 20,
+    rating:4.7,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11952910/2021/4/23/9eb5d27f-860b-4d2a-ac2a-ebd9139c75491619182890615-HRX-by-Hrithik-Roshan-Men-Grey-Core-Sports-Shoe-467161918288-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "HRX by Hrithik Roshan",
+    productName: "Men Grey Street Run 2.0 Running Shoe",
+    price: 799,
+    discount: 50,
+    rating:4.1,
+    categoryName: "Shoes",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11954122/2021/6/16/002632a8-df3e-4f56-bcb0-73cf5c97ad851623846620916-HRX-by-Hrithik-Roshan-Men-Grey-Street-Run-20-Running-Shoe-73-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Jompers",
+    productName: "Women Green & Blue Printed A-Line Kurta",
+    price: 675,
+    discount: 48,
+    rating:4.1,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10265935/2019/7/26/2367e212-2ac8-4c42-98ed-6dc3c79261371564119180239-Jompers-Women-Green--Blue-Printed-Anarkali-Kurta-78215641191-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Soch",
+    productName: "Women Yellow Geometric Printed Kurta",
+    price: 798,
+    discount: 18,
+    rating:5,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17232036/2022/2/21/a1458816-96ca-4ff9-b506-c4940d59661b1645435829704SochWomenYellowGeometricPrintedKeyholeNeckThreadWorkKurta1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Anubhutee",
+    productName: "Women Navy Blue & White Bandhej Printed Straight Kurta",
+    price: 594,
+    discount: 65,
+    rating:3.5,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10513952/2022/2/22/1abcd853-3d8e-44b7-a0aa-28676309879e1645512977064AnubhuteeWomenNavyBlueWhiteBandhejPrintedStraightKurta1.jpg"
+  },
+  
+  {
+    _id: uuid(),
+    company: "Taavi",
+    productName: "Women Red & White Ikat Woven Design A-Line Dress with Shirt Collar & Gathers",
+    price: 449,
+    discount: 50,
+    rating:4,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/10128409/2021/2/11/2f5cf124-f138-47e4-be79-a28b2db1f5411613042971145-Taavi-Women-Red--White-Ikat-Woven-Design-A-Line-Dress-with-S-2.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "See Designs",
+    productName: "Women Blue & White Khari Print Straight Kurta",
+    price: 284,
+    discount: 75,
+    rating:0.5,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/11519290/2020/2/25/19cf3192-3ca7-46a4-88e6-e5ae137995c01582624360192-See-Designs-Women-Blue--White-Printed-Straight-Kurta-1011582-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Anouk",
+    productName: "Women Pink Printed Straight Kurta",
+    price: 99,
+    discount: 40,
+    rating:1.5,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2322979/2018/2/13/11518506064968-Anouk-Women-Peach-Coloured-Printed-Straight-Kurta-6851518506064751-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "KISAH",
+    productName: "Men Multicoloured Ethnic Motifs Printed Kurta",
+    price: 849,
+    discount: 50,
+    rating:5,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/15584420/2022/3/12/418dc892-93b7-47dd-a114-c1247b9d172d1647065967138-KISAH-Men-Multicoloured-Ethnic-Motifs-Printed-Kurta-89164706-6.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "KISAH",
+    productName: "Women Green Shoulder Strap Kurta",
+    price: 349,
+    discount: 80,
+    rating:3,
+    categoryName: "Kurtas",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14499342/2022/3/3/c05f6d54-e210-4d61-8e0a-be55e4346f3e1646287677106-Ode-by-House-of-Pataudi-Women-Green-Shoulder-Strap-Kurta-836-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Saree mall",
+    productName: "Burgundy Striped Poly Georgette Party Wear Saree with Matching Blouse",
+    price: 359,
+    discount: 66,
+    rating:4.3,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14984816/2021/8/26/6f00fe6c-258e-44da-907d-5a91f16d21491629966207295-Saree-Mall-Wine-Georgette-Ethnic-Solid-Party-Wear-Saree-with-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Tikhi Imli",
+    productName: "Delicate Blue and Maroon Printed Organza Saree with Solid Border",
+    price: 129,
+    discount: 50,
+    rating:2.3,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/13919230/2021/7/8/309807a1-3122-46f1-a42c-5586a06297ac1625719615892-Floral-Organza-saree-with-SatinBorder-9811625719615378-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Charukriti",
+    productName: "Magenta Woven Design Jamdani Saree",
+    price: 712,
+    discount: 14,
+    rating:.7,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17124104/2022/2/10/325bfc28-c2f1-4e6a-ab92-1b59254896c51644499218268CharukritiWomenMagentaEmbellished1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Charukriti",
+    productName: "Magenta Woven Design Jamdani Saree",
+    price: 990,
+    discount: 95,
+    rating:5,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17234736/2022/2/21/3c809161-f23f-4b53-9936-7d718b9819ce1645452374124Sarees1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Charukriti",
+    productName: "Magenta & Black Printed Pure Silk Saree",
+    price: 503,
+    discount: 16,
+    rating:2.5,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17135614/2022/2/11/6adb9ec4-e003-49ff-87a9-e7b0c903c8831644579272076CharukritiWomenMagentaEmbellished1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Sangria",
+    productName: "Navy Blue Solid Saree",
+    price: 853,
+    discount: 56,
+    rating:3.5,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2021/5/25/a2c32671-3cb9-46cc-81c1-a722ddbf4c1e1621929685104-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Tikhi Imli",
+    productName: "Navy Blue Poly-Georgette Saree with Solid border",
+    price: 559,
+    discount: 50,
+    rating:4.8,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16189020/2021/12/6/da53ff1d-b79b-4329-8cdd-efc66d01bf5c1638787114831-Navy-Blue-Mirror-Work-Ruffle-Saree-571638787114246-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Mitera",
+    productName: "Black & Blue Poly Crepe Printed Saree",
+    price: 973,
+    discount: 70,
+    rating:4,
+    categoryName: "Sarees",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/productimage/2019/12/14/8251f9a2-101f-4a4d-bc9c-73a36570573f1576285313782-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "HIGHLANDER",
+    productName: "Highlander Black Chinos",
+    price: 599,
+    discount: 40,
+    rating:4.2,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1380186/2019/3/15/25c47c73-44ed-4939-89a2-eaefce6733c81552645766838-Highlander-Black-Solid-Chinos-801552645765097-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Roadster",
+    productName: "Women Olive Green & Black Slim Fit Printed Chinos",
+    price: 659,
+    discount: 70,
+    rating:3,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/2217969/2017/11/14/11510650694129-Roadster-Women-Olive-Green--Black-Slim-Fit-Printed-Regular-Trousers-3401510650693998-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "SASSAFRAS",
+    productName: "Women Olive Green Pure Cotton Straight Trousers",
+    price: 764,
+    discount: 55,
+    rating:2,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/13843132/2021/3/23/98644bfe-cdd7-47ff-b355-2b65473dfe981616490669259-SASSAFRAS-Women-Olive-Green-Regular-Fit-Solid-Pure-Cotton-St-2.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Louis Philippe Ath.Work",
+    productName: "Louis Philippe Ath Work Men Navy Blue Solid Comfort Tapered Fit Low-Rise Trousers",
+    price: 973,
+    discount: 15,
+    rating:1,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/16890232/2022/2/22/06119a79-2e99-4474-8012-ac8a3287b7f01645534396692-Louis-Philippe-AthWork-Men-Trousers-4171645534396107-1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "STREET 9",
+    productName: "Men Grey Solid Slim Fit Low-Rise Wrinkle Free Formal Trousers",
+    price: 429,
+    discount: 35,
+    rating:.6,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17431386/2022/3/8/17d761b7-d9f1-49a0-8f9d-3eaf46b9ef9b1646719043392STREET9WomenGreenLooseFitHigh-RiseCulottesTrousers1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Louis Philippe",
+    productName: "Women Green Loose Fit High-Rise Culottes Trousers",
+    price: 993,
+    discount: 75,
+    rating:4.6,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14305734/2022/2/1/8a688e19-e30c-48f9-9d9c-23e44ac0d35f1643718734758-Louis-Philippe-Men-Trousers-1451643718734171-2.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "H&M",
+    productName: "Women Beige Solid Ankle-Length Trousers",
+    price: 93,
+    discount: 99,
+    rating:3.6,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/17465196/2022/3/10/bb1f1fe7-17a3-4bff-ab02-ef630f8126641646901474929Ankle-lengthtrousers1.jpg"
+  },
+  {
+    _id: uuid(),
+    company: "Louis Philippe",
+    productName: "Men Navy Blue Slim Fit Formal Trousers",
+    price: 723,
+    discount: 35,
+    rating:5,
+    categoryName: "Trousers",
+    image:"https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14602076/2022/2/25/d596b3f7-8a9e-465a-9319-e2eb2af65bb81645790315287-Louis-Philippe-Men-Black-Slim-Fit-Formal-Trousers-8571645790-2.jpg"
+  },
+  
 ];
