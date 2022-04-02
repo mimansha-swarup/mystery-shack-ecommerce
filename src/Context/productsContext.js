02aa924a31ce014b7b3d2df871463e6b27ea55e9
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-02 07:19:01 +0530

    fix: bug

--- src/Context/productsContext.js
+++ src/Context/productsContext.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 import { createContext, useContext, useEffect, useState } from "react";
 import { productsApi } from "../Helper/Api/api";
+import { useAuth } from "./authContext";
 
 const productsContext = createContext();
 
@@ -9,22 +10,27 @@ export const useProducts = () => useContext(productsContext);
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  const [status, setStatus] = useState({ isLoading: false, wrror: "" });
+  const [status, setStatus] = useState({ isLoading: false, error: "" });
+  const {authState} =useAuth()
 
   useEffect(() => {
-    (async () => {
-      setStatus({ isLoading: true, error: "" });
-      const response = await axios.get(productsApi);
-      if (response.status === 200) {
-        setProducts(response.data.products);
-        setStatus({ isLoading: false, error: "" });
-      }
-      try {
-      } catch (error) {
-        setStatus({ isLoading: false, error: error.message });
-      }
-    })();
-  }, []);
+    if(authState?.token){
+
+      
+      (async () => {
+        setStatus({ isLoading: true, error: "" });
+        const response = await axios.get(productsApi);
+        if (response.status === 200) {
+          setProducts(response.data.products);
+          setStatus({ isLoading: false, error: "" });
+        }
+        try {
+        } catch (error) {
+          setStatus({ isLoading: false, error: error.message });
+        }
+      })();
+    }
+    }, [authState?.token]);
 
   return (
     <productsContext.Provider value={{ products, status }}>
