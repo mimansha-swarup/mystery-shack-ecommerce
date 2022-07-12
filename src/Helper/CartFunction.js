534d68292e7ad51ad5084baa0bbe6f2f8fca0f37
mimansha-swarup <mimanshaswarup@gmail.com>
2022-07-12 15:58:54 +0530

    fix: discount price

--- src/Helper/CartFunction.js
+++ src/Helper/CartFunction.js
@@ -1,8 +1,17 @@
+import { DiscountedPrice } from "./DIscountedPrice";
+
 export const cartTotalPrice = (cartList) =>
   cartList.reduce((sum, product) => sum + product.price * product.quantity, 0);
 
 export const cartTotalDiscount = (cartList) =>
   cartList.reduce(
     (sum, product) => sum + product.discount * product.quantity,
+    0
+  );
+
+export const cartOrignalValue = (cartList) =>
+  cartList.reduce(
+    (sum, product) =>
+      sum + DiscountedPrice(product.price, product.discount) * product.quantity,
     0
   );
